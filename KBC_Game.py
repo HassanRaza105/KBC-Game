fe4f2578ba8b63897acd64f268cda2f37e3d28ec
MUHAMMAD HASSAN RAZA <hassan105raza@gmail.com>
2025-06-16 12:31:13 +0500

    Initial commit

---
+++ KBC_Game.py
@@ -0,0 +1,56 @@
+questionsList = [
+    "What is capital of pakistan?",
+    "What is capital of Punjab",
+    "What is capital of Sindh?",
+    "What is capital of KPK?",
+    "What is capital of Bloachistan?",
+]
+
+answerList = ["islamabad", "lahore", "karachi", "peshawar", "quetta"]
+
+optionList = [
+    ["peshawar", "karachi", "lahore", "rawalpindi", "islamabad"],
+    ["gujranwala", "multan", "sialkot", "lahore", "faisalabad"],
+    ["karachi", "sukkur", "hyderabad", "mirpur khas", "larkana"],
+    ["abbottabad", "dg khan", "peshawar", "kohat", "mardan"],
+    ["sibi", "quetta", "gwadar", "khuzdar", "zob"],
+]
+
+prize = list((n + 1) * 100 for n in range(len(questionsList)))
+
+wonAmount = 0
+
+for questionNo in range(len(questionsList)):
+    print("Won amount so far: ", wonAmount)
+    print(
+        "\nFor the prize money of",
+        prize[questionNo],
+        "\nQuestion no.",
+        questionNo + 1,
+        ":",
+        questionsList[questionNo],
+    )
+    gameEnd = False
+    print("Your options are: ")
+    for options in range(len(optionList[questionNo])):
+        print(options + 1, optionList[questionNo][options], end=" ")
+    while True:
+        try:
+            userChoice = int(
+                input("\nPlease choose an answer(Enter one of the option no): ")
+            )
+            if userChoice not in range(len(optionList[questionNo]) + 1):
+                print("Invalid choice!")
+            elif answerList[questionNo] == optionList[questionNo][userChoice - 1]:
+                print("good!")
+                wonAmount = wonAmount + prize[questionNo]
+                break
+            else:
+                print("In correct! Game Over.")
+                gameEnd = True
+                break
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+    if gameEnd:
+        break
+print("Total amount you won is:", wonAmount)
---
+++ README.md
@@ -0,0 +1,2 @@
+# KBC-Game
+A simple Python quiz game inspired by *KBC* that tests knowledge of Pakistan's provincial capitals.
